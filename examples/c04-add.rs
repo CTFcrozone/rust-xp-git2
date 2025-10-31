90eef24a92e6664d6bfdf1b3d06c30cc5598bea6
χ <voidbyte@null.net>
2025-10-31 17:03:24 +0100

    . c04-add

---
+++ examples/c04-add.rs
@@ -0,0 +1,24 @@
+use git2::{Repository, RepositoryInitOptions};
+use std::path::{Path, PathBuf};
+
+pub type Result<T> = core::result::Result<T, Error>;
+pub type Error = Box<dyn std::error::Error>; // For early dev.
+
+const REPO_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/test/repo1/");
+const REMOTE_ORIGIN: &str = "https://github.com/CTFcrozone/xp-git2-test-repo.git";
+
+fn main() -> Result<()> {
+	let repo = Repository::open(REPO_PATH)?;
+
+	let mut index = repo.index()?;
+
+	let paths: &[&str] = &["file.rs"];
+
+	for path in paths {
+		index.add_path(Path::new(path))?;
+	}
+
+	index.write()?;
+
+	Ok(())
+}
